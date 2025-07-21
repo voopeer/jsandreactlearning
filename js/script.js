b06c377ab2e2f0f89532a4addc4a73a381f7596d
Mykhailo <mishaoberemko@gmail.com>
2025-07-21 12:33:43 +0300

    first js console-app

--- js/script.js
+++ js/script.js
@@ -1,9 +1,19 @@
-console.log('arr' + ' - object');
+const numberOfFilms = +prompt('Скільки фільмів Ви вже подивились?', '');
 
-let incr = 10,
-    decr = 10;
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
 
-incr++;
-decr--;
-console.log(incr);
-console.log(decr);
+const a = prompt('Останній переглянутий фільм?', ''),
+      b = prompt('Як Ви його оціните?', ''),
+      c = prompt('Останній переглянутий фільм?', ''),
+      d = prompt('Як Ви його оціните?', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
