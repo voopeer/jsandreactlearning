4b3dbfe9be88faf68f5c26ebf36be3a4910fa9a2
Mykhailo <mishaoberemko@gmail.com>
2025-07-21 18:26:05 +0300

    logical operators lesson

--- js/script.js
+++ js/script.js
@@ -1,32 +1,22 @@
-if ( 4 == 9) {
-    console.log('Ok!');
+const hamburger = 3;
+const fries = 3;
+const cola = 0;
+const nuggets = 2;
+
+// console.log(hamburger === 3 && cola && fries);
+
+// console.log(1 && 0);
+// console.log(1 && 5);
+// console.log(null && 5);
+// console.log(0 && 'fdfslkdfks');
+
+if (hamburger === 3 || cola == 2 || fries === 3 && nuggets) {
+    console.log('We are not hungry');
 } else {
-    console.log('Error');
-}
-
-const num = 50;
-
-if (num < 49) {
-    console.log('Error');
-} else if (num > 100) {
-    console.log('Much');
-} else {
-    console.log('OK!');
+    console.log('We are go out')
 }
 
 
+let johnReport, alexReport, samReport, mariaReport = 'done';
 
-switch (num) {
-    case 49: 
-        console.log('False');
-        break;
-    case 100: 
-        console.log('False');
-        break;
-    case 50: 
-        console.log('True');
-        break;
-    default:
-        console.log('No in this moment');
-        break;
-}
+console.log(johnReport || alexReport || samReport || mariaReport);
